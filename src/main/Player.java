900791448d11a0cf2da06051dfbe3113a22a483f
idasag <idasag@stud.ntnu.no>
2014-09-17 12:33:01 +0200

    Player-klasse endret
    Lagt til gettere og settere

---
+++ src/main/Player.java
@@ -0,0 +1,36 @@
+package main;
+
+public class Player {
+
+	String name;
+	int age;
+	String uname;
+	
+	public void newPlayer(String name, int age, String uname){
+		setName(name);
+		setAge(age);
+		setUname(uname);
+	}
+	
+	public void setName(String name){
+		this.name=name;
+	}
+
+	public String getName(){
+		return this.name;
+	}
+	public void setAge(int age){
+		this.age=age;
+	}
+
+	public int getAge(){
+		return this.age;
+	}
+	public void setUname(String uname){
+		this.uname=uname;
+	}
+
+	public String getUname(){
+		return this.uname;
+	}
+}
