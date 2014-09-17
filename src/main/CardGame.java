323ab0472383f2708328f8685941d52e26c84251
Marthe Oline <martheos@stud.ntnu.no>
2014-09-17 12:37:28 +0200

    hei
    her er cardgame

--- src/main/CardGame.java
+++ src/main/CardGame.java
@@ -2,4 +2,16 @@ package main;
 
 public class CardGame {
 
+	boolean run;
+	Card c;
+	
+	public CardGame() {
+		c = new Card;
+		run = true;
+	}
+	public static void main(String[] args) {
+		CardGame cg = new CardGame();
+		cg.run();
+	}
+	
 }
