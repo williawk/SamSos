bb6afa2fd533b33375017d04461f55ea028909da
Marthe Oline <martheos@stud.ntnu.no>
2014-09-17 12:36:25 +0200

    litt
    i card

--- src/main/Card.java
+++ src/main/Card.java
@@ -1,5 +1,15 @@
 package main;
 
+import java.util.Scanner;
+
 public class Card {
+	
+	public void CreateQandA(){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Question");
+		String Q = sc.next();
+		System.out.println("Answer");
+		String A = sc.next();
+	}
 
 }
